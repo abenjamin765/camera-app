37b7a0c95757ec72edbace8c5e095be815196359
Aaron Benjamin <abenjamin765@gmail.com>
2018-04-20 10:51:05 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
 // Set constraints for the video stream
 var constraints = { video: { facingMode: "user" }, audio: false };
+
 // Define constants
 const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
     cameraSensor = document.querySelector("#camera--sensor"),
     cameraTrigger = document.querySelector("#camera--trigger")
+
 // Access the device camera and stream to cameraView
 function cameraStart() {
     navigator.mediaDevices
         .getUserMedia(constraints)
         .then(function(stream) {
+            track = stream.getTracks()[0];
             cameraView.srcObject = stream;
         })
         .catch(function(error) {
             console.error("Oops. Something is broken.", error);
         });
 }
+
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    track.stop();
 };
+
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);

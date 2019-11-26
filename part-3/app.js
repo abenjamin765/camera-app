14351cf0edb32c744a9a324bc41bfea035c3f40f
Don Wei <donwei@ebay.com>
2019-11-25 21:39:28 -0800

    try

--- part-3/app.js
+++ part-3/app.js
@@ -1,5 +1,5 @@
 // Set constraints for the video stream
-var constraints = { video: { facingMode: "user" }, audio: false };
+var constraints = { video: true, audio: false };
 var track = null;
 
 // Define constants

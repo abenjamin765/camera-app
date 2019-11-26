3f45d1b51e50f237343e2530c37b5a707232f866
Serene Xiaopu Deng <xiaopudeng@cca.edu>
2019-11-25 16:46:13 -0800

    add monalisa

---
+++ part-3/app.js
@@ -0,0 +1,41 @@
+// Set constraints for the video stream
+var constraints = { video: { facingMode: "user" }, audio: false };
+var track = null;
+
+// Define constants
+const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger");
+    commentSubmit = document.querySelector("#comment--button");
+
+
+// Access the device camera and stream to cameraView
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+            track = stream.getTracks()[0];
+            cameraView.srcObject = stream;
+        })
+}
+
+commentSubmit.onclick = function() {
+    var x = document.getElementById("comment--input").value;
+    var element = document.createElement("button");
+    element.innerHTML = x;
+    document.getElementById("bullets").appendChild(element);
+}
+
+// Take a picture when cameraTrigger is tapped
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+    // track.stop();
+};
+
+// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
